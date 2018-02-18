5712a8e205394a55b610f8d6feb78eba45714b79
Michael Farrell <micolous+git@gmail.com>
2018-02-18 14:18:33 +1100

    natives: test that we got a good result from HardnestedTest

--- natives/src/androidTest/java/au/id/micolous/andprox/natives/test/HardnestedTest.java
+++ natives/src/androidTest/java/au/id/micolous/andprox/natives/test/HardnestedTest.java
@@ -1,8 +1,8 @@
 package au.id.micolous.andprox.natives.test;
 
+import android.support.test.filters.FlakyTest;
 import android.test.AndroidTestCase;
-import android.test.suitebuilder.annotation.MediumTest;
-import android.test.suitebuilder.annotation.Suppress;
+import android.test.suitebuilder.annotation.LargeTest;
 
 import au.id.micolous.andprox.natives.Natives;
 import au.id.micolous.andprox.natives.Resources;
@@ -10,13 +10,17 @@ import au.id.micolous.andprox.natives.Resources;
 /**
  * Test hardnested attacks.
  *
- * This test is flaky due to high RAM requirements (~2GB).
+ * This test is flaky due to high RAM requirements (~2GB). It crashes in the Android Emulator.
  */
-@Suppress
+@FlakyTest
 public class HardnestedTest extends AndroidTestCase {
+    private LogSink mLogSink;
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+
+        mLogSink = new LogSink();
 
         // Hardnested attacks need some resources.
         if (!Resources.extractPM3Resources(getContext())) {
@@ -26,11 +30,13 @@ public class HardnestedTest extends AndroidTestCase {
         // Now we can actually init
         Natives.initProxmark();
         Natives.unsetSerialPort();
-
     }
 
-    @MediumTest
+    @LargeTest
     public void testHardnested() {
+        mLogSink.reset();
         Natives.sendCmd("hf mf hardnested t 1 000000000000");
+        String logLine = mLogSink.findInLogLines("Brute force phase completed. Key found: 000000000000");
+        assertNotNull(logLine);
     }
 }
---
+++ natives/src/androidTest/java/au/id/micolous/andprox/natives/test/LogSink.java
@@ -0,0 +1,44 @@
+package au.id.micolous.andprox.natives.test;
+
+import android.test.suitebuilder.annotation.Suppress;
+
+import java.util.LinkedList;
+
+import au.id.micolous.andprox.natives.Natives;
+
+/**
+ * Allows unit tests to capture PrintAndLog outputs.
+ */
+@Suppress
+public class LogSink implements Natives.PrinterArgs {
+    protected LinkedList<String> mLogLines;
+
+    public void reset() {
+        mLogLines = new LinkedList<>();
+    }
+
+    public LogSink() {
+        reset();
+        Natives.registerPrintAndLogHandler(this);
+    }
+
+    @Override
+    public void onPrint(String log) {
+        mLogLines.add(log);
+    }
+
+    /**
+     * Finds a "needle" in the log lines. Returns null if not found. Case sensitive.
+     * @param needle Case sensitive string to match on.
+     * @return Complete log line that matched.
+     */
+    public String findInLogLines(CharSequence needle) {
+        for (String l : mLogLines) {
+            if (l.contains(needle)) {
+                return l;
+            }
+        }
+
+        return null;
+    }
+}
