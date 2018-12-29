590cf9ca91726010d8576db5c9cf14bdaea9ee7a
Michael Farrell <micolous+git@gmail.com>
2018-12-29 20:07:03 +1000

    Move all licensing bits into assets

--- app/src/main/java/au/id/micolous/andprox/components/LicenseFragment.java
+++ app/src/main/java/au/id/micolous/andprox/components/LicenseFragment.java
@@ -29,13 +29,25 @@
  */
 package au.id.micolous.andprox.components;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 import au.id.micolous.andprox.R;
 
@@ -43,6 +55,7 @@ import au.id.micolous.andprox.R;
  * Fragment which displays all the license acknowledgements.
  */
 public class LicenseFragment extends Fragment {
+    private static final String TAG = LicenseFragment.class.getSimpleName();
 
     public LicenseFragment() {
         // Required empty public constructor
@@ -66,162 +79,49 @@ public class LicenseFragment extends Fragment {
         TextView t = v.findViewById(R.id.tvLicenseText);
 
         // These strings are deliberately not localised.
-        t.setText(LICENSE_INTRO);
+        readLicenseTextFromAsset(t, "NOTICE.AndProx.txt");
 
-        t.append(PROXMARK3);
-        t.append(ZLIB);
-        t.append(JANSSON);
-        t.append(LUA);
-        t.append(USB_SERIAL_FOR_ANDROID);
-        t.append(GRAPHVIEW);
+        readLicenseTextFromAsset(t, "third_party/NOTICE.proxmark3.txt");
+        readLicenseTextFromAsset(t, "third_party/NOTICE.jansson.txt");
+        readLicenseTextFromAsset(t, "third_party/NOTICE.lua.txt");
+        readLicenseTextFromAsset(t, "third_party/NOTICE.zlib.txt");
+
+        readLicenseTextFromAsset(t, "third_party/NOTICE.GraphView.txt");
+        readLicenseTextFromAsset(t, "third_party/NOTICE.usb-serial-for-android.txt");
+        readLicenseTextFromAsset(t, "third_party/NOTICE.webrtc.txt");
 
         return v;
     }
 
-    static final String LICENSE_INTRO = "AndProx\n"
-        + "Copyright 2016-2018 Michael Farrell <micolous@gmail.com> and contributors\n"
-        + "\n"
-        + "This program is free software: you can redistribute it and/or modify "
-        + "it under the terms of the GNU General Public License as published by "
-        + "the Free Software Foundation, either version 3 of the License, or "
-        + "(at your option) any later version.\n"
-        + "\n"
-        + "This program is distributed in the hope that it will be useful, "
-        + "but WITHOUT ANY WARRANTY; without even the implied warranty of "
-        + "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
-        + "GNU General Public License for more details.\n"
-        + "\n"
-        + "You should have received a copy of the GNU General Public License "
-        + "along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
-        + "\n"
-        + "Under section 7 of the GNU General Public License v3, the following additional terms apply to this program:\n"
-        + "\n"
-        + "(b) You must preserve reasonable legal notices and author attributions in the program.\n"
-        + "(c) You must not misrepresent the origin of this program, and need to mark modified versions in reasonable ways as different from the original version (such as changing the name and logos).\n"
-        + "(d) You may not use the names of licensors or authors for publicity purposes, without explicit written permission.\n"
-        + "\n"
-        + "The source code is available at https://github.com/AndProx/AndProx/\n\n\n";
+    private void readLicenseTextFromAsset(@NonNull TextView t, @NonNull String path) {
+        final Context ctx = getContext();
+        if (ctx == null) {
+            licenseError(t, path);
+            return;
+        }
 
-    static final String PROXMARK3 = "Proxmark3\n"
-        + "Copyright 2007-2018 proxmark3 contributors\n"
-        + "\n"
-        + "This program is free software; you can redistribute it and/or modify "
-        + "it under the terms of the GNU General Public License as published by "
-        + "the Free Software Foundation; either version 2 of the License, or "
-        + "(at your option) any later version.\n"
-        + "\n"
-        + "This program is distributed in the hope that it will be useful, "
-        + "but WITHOUT ANY WARRANTY; without even the implied warranty of "
-        + "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
-        + "GNU General Public License for more details.\n"
-        + "\n"
-        + "You should have received a copy of the GNU General Public License "
-        + "along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
-        + "\n"
-        + "The modified source code is available in third_party/proxmark3/ in the AndProx repository.\n\n\n";
+        final AssetManager assets = ctx.getAssets();
+        if (assets == null) {
+            licenseError(t, path);
+            return;
+        }
 
-    static final String USB_SERIAL_FOR_ANDROID = "This software contains usb-serial-for-android, an Android userspace USB serial device driver.\n"
-        + "Copyright 2011-2012 Google Inc\n"
-        + "\n"
-        + "This program is free software; you can redistribute it and/or modify "
-        + "it under the terms of the GNU Lesser General Public License as published by "
-        + "the Free Software Foundation; either version 2.1 of the License, or "
-        + "(at your option) any later version.\n"
-        + "\n"
-        + "This program is distributed in the hope that it will be useful, "
-        + "but WITHOUT ANY WARRANTY; without even the implied warranty of "
-        + "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
-        + "GNU General Public License for more details.\n"
-        + "\n"
-        + "You should have received a copy of the GNU General Public License "
-        + "along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
-        + "\n"
-        + "The source code is available in third_party/usb-serial-for-android/ in the AndProx repository.\n\n\n";
+        try {
+            final InputStream s = assets.open(path, AssetManager.ACCESS_RANDOM);
+            final Scanner sc = new Scanner(s, "UTF-8");
+            final String o = sc.useDelimiter("\\A").hasNext() ? sc.next() : "";
 
-    static final String ZLIB = "zlib\n" +
-        "Copyright 1995-2013 Jean-loup Gailly and Mark Adler\n"+
-        "\n"+
-        "This software is provided 'as-is', without any express or implied " +
-        "warranty.  In no event will the authors be held liable for any damages "+
-        "arising from the use of this software.\n"+
-        "\n"+
-        "Permission is granted to anyone to use this software for any purpose, "+
-        "including commercial applications, and to alter it and redistribute it "+
-        "freely, subject to the following restrictions:\n"+
-        "\n"+
-        "  1. The origin of this software must not be misrepresented; you must not\n"+
-        "     claim that you wrote the original software. If you use this software\n"+
-        "     in a product, an acknowledgment in the product documentation would be\n"+
-        "     appreciated but is not required.\n"+
-        "  2. Altered source versions must be plainly marked as such, and must not be\n"+
-        "     misrepresented as being the original software.\n"+
-        "  3. This notice may not be removed or altered from any source distribution.\n"+
-        "\n"+
-        "Jean-loup Gailly        Mark Adler\n"+
-        "jloup@gzip.org          madler@alumni.caltech.edu\n\n"
-        + "The source code is available in third_party/proxmark3/zlib/ in the AndProx repository.\n\n\n";
+            t.append(o);
+            t.append("\n\n");
+        } catch (IOException e) {
+            //noinspection StringConcatenation
+            Log.w(TAG, "Error reading license: " + path, e);
+            licenseError(t, path);
+        }
 
-    static final String JANSSON = "This software conatins jansson, a library for JSON parsing.\n" +
-            "Copyright (c) 2009-2018 Petri Lehtinen <petri@digip.org>\n" +
-            "\n" +
-            "Permission is hereby granted, free of charge, to any person obtaining a copy " +
-            "of this software and associated documentation files (the \"Software\"), to deal " +
-            "in the Software without restriction, including without limitation the rights " +
-            "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell " +
-            "copies of the Software, and to permit persons to whom the Software is " +
-            "furnished to do so, subject to the following conditions:\n" +
-            "\n" +
-            "The above copyright notice and this permission notice shall be included in " +
-            "all copies or substantial portions of the Software.\n" +
-            "\n" +
-            "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR " +
-            "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " +
-            "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE " +
-            "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER " +
-            "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, " +
-            "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN " +
-            "THE SOFTWARE.\n" +
-            "The source code is available in third_party/proxmark3/client/jannson/ in the AndProx repository.\n\n\n";
+    }
 
-    static final String LUA = "This software contains Lua, a light-weight scripting language.\n" +
-            "Copyright © 1994–2018 Lua.org, PUC-Rio.\n" +
-            "\n" +
-            "Permission is hereby granted, free of charge, to any person obtaining a copy of this " +
-            "software and associated documentation files (the \"Software\"), to deal in the " +
-            "Software without restriction, including without limitation the rights to use, copy, " +
-            "modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, " +
-            "and to permit persons to whom the Software is furnished to do so, subject to the " +
-            "following conditions:\n" +
-            "\n" +
-            "The above copyright notice and this permission notice shall be included in all " +
-            "copies or substantial portions of the Software.\n" +
-            "\n" +
-            "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR " +
-            "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS " +
-            "FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR " +
-            "COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER " +
-            "IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION " +
-            "WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n" +
-            "\n" +
-            "The source code is available in third_party/proxmark3/liblua/ in the AndProx " +
-            "repository.\n\n\n";
-
-    static final String GRAPHVIEW = "This software contains GraphView, a library for building graphs on Android.\n"
-        + "Copyright 2014 Jonas Gehring\n"
-        + "\n"
-        + "This program is free software; you can redistribute it and/or modify "
-        + "it under the terms of the GNU General Public License as published by "
-        + "the Free Software Foundation; either version 2 of the License, with the "
-        + "\"Linking Exception\", which can be found at the license.txt file in "
-        + "third_party/GraphView/.\n"
-        + "\n"
-        + "This program is distributed in the hope that it will be useful, "
-        + "but WITHOUT ANY WARRANTY; without even the implied warranty of "
-        + "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
-        + "GNU General Public License for more details.\n"
-        + "\n"
-        + "You should have received a copy of the GNU General Public License "
-        + "along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
-        + "\n"
-        + "The modified source code is available in third_party/GraphView/ in the AndProx repository.\n\n\n";
+    private void licenseError(@NonNull TextView t, @NonNull String path) {
+        t.append("\n\n** Error reading license notice from " + path + "\n\n");
+    }
 }
