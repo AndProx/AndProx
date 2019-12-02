e130e95fe24885f82e348850ad99dcb385d80161
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2019-12-01 22:42:43 -0500

    extracted format device from AndProxApplication

--- app/src/main/java/au/id/micolous/andprox/AndProxApplication.java
+++ app/src/main/java/au/id/micolous/andprox/AndProxApplication.java
@@ -30,17 +30,7 @@
 
 package au.id.micolous.andprox;
 
-import android.app.ActivityManager;
 import android.app.Application;
-import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.util.Log;
-
-import java.util.Locale;
-
-import au.id.micolous.andprox.natives.Natives;
 
 /**
  * AndProx application reference.
@@ -49,7 +39,7 @@ public class AndProxApplication extends Application {
 
     private static AndProxApplication sInstance;
     private String mExtraDeviceInfo = "";
-    private static final String TAG = "AndProxApplication";
+
     private boolean mProxmarkDetected = false;
     private boolean mOldProxmarkDetected = false;
 
@@ -79,85 +69,6 @@ public class AndProxApplication extends Application {
 
     public static AndProxApplication getInstance() {
         return sInstance;
-    }
-
-    private static String formatMemoryInfo(ActivityManager.MemoryInfo mi) {
-        if (mi == null) {
-            return "null";
-        }
-
-        return String.format(Locale.ENGLISH, "%s (%s free)",
-                Utils.formatBytes(mi.totalMem),
-                Utils.formatBytes(mi.availMem));
-    }
-
-    /**
-     * Dumps all device information that is useful for debugging AndProx.
-     *
-     * This always returns strings in English, and is only ever used for the SysInfoActivity. This
-     * is so that bug reports are readable by the developers. ;)
-     */
-    public static String getDeviceInfo(Context ctx) {
-        ActivityManager.MemoryInfo mi = null;
-
-        try {
-            ActivityManager am = (ActivityManager) ctx.getSystemService(Context.ACTIVITY_SERVICE);
-            if (am != null) {
-                mi = new ActivityManager.MemoryInfo();
-                am.getMemoryInfo(mi);
-            }
-        } catch (Exception e) {
-            Log.w(TAG, "Error getting memory information", e);
-            mi = null;
-        }
-
-        return String.format(Locale.ENGLISH,
-                        "AndProx version: %s\n" +
-                        "PM3 Client version: %s\n" +
-                        "Build timestamp: %s\n" +
-                        "Model: %s (%s)\n" +
-                        "Product: %s\n" +
-                        "Manufacturer: %s (%s)\n" +
-                        "RAM: %s\n" +
-                        "Android OS: %s (API %d)\n" +
-                        "Android Build: %s\n\n" +
-                        "USB Host: %s\n" +
-                        "%s", // extra device info
-                // Version:
-                getVersionString(),
-                Natives.getProxmarkClientVersion(),
-                Natives.getProxmarkClientBuildTimestamp(),
-                // Model:
-                Build.MODEL,
-                Build.DEVICE,
-                Build.PRODUCT,
-                // Manufacturer / brand:
-                Build.MANUFACTURER,
-                Build.BRAND,
-                // RAM:
-                formatMemoryInfo(mi),
-                // OS:
-                Build.VERSION.RELEASE,
-                Build.VERSION.SDK_INT,
-                // Build:
-                Build.ID,
-                // USB:
-                hasUsbHostSupport() ? "yes" : "no",
-                getInstance().mExtraDeviceInfo);
-    }
-
-    public static String getVersionString() {
-        PackageInfo info = getPackageInfo();
-        return String.format("%s (%s)", info.versionName, info.versionCode);
-    }
-
-    private static PackageInfo getPackageInfo() {
-        try {
-            AndProxApplication app = AndProxApplication.getInstance();
-            return app.getPackageManager().getPackageInfo(app.getPackageName(), 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            throw new RuntimeException(e);
-        }
     }
 
 }
