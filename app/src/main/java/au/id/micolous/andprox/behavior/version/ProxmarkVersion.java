b301b793425cf1d49c03621ea4f1dda91afd8061
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2019-12-02 11:43:57 -0500

    deleted proxmark parser logic from ProxmarkVersion class

--- app/src/main/java/au/id/micolous/andprox/behavior/version/ProxmarkVersion.java
+++ app/src/main/java/au/id/micolous/andprox/behavior/version/ProxmarkVersion.java
@@ -33,13 +33,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Locale;
-import java.util.TimeZone;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import static au.id.micolous.andprox.AndProxApplication.allowAllProxmarkDevices;
+import au.id.micolous.andprox.device.ISharedPreferences;
 
 /**
  * Version detection routine for Proxmark3 firmware.
@@ -62,145 +57,10 @@ public class ProxmarkVersion {
         CHINA,
     }
 
-    // 2000-01-31 23:59:59
-    // 2000/01/31 23:59: 1
-    // 2000/ 1/31 at  9:59:59
-    private static Pattern ISO_DATE_MATCHER = Pattern.compile(
-            "(\\d{4})[-\\/]\\s?(\\d{1,2})[-\\/]\\s?(\\d{1,2}) (at )?\\s?(\\d{1,2}):\\s?(\\d{1,2}):\\s?(\\d{1,2})");
+    private ISharedPreferences preferences;
 
-    // v3.0.1
-    // v3.0.1-1234-g1234567
-    private static Pattern PM3_VERSION_MATCHER = Pattern.compile(
-            "v(\\d+)\\.(\\d+)\\.(\\d+)(-(\\d+)-g([\\da-f]{7}))?"
-    );
-
-    private ProxmarkVersion() {}
-
-    /**
-     * Searches for something that looks like an ISO 8601 datetime string, and attempts to parse it.
-     *
-     * @param s The string to parse.
-     * @return The value of the parsed Calendar, or null on error.
-     */
-    @Nullable
-    private static Calendar parseIsoDateTime(@NonNull String s) {
-        try {
-            Matcher m = ISO_DATE_MATCHER.matcher(s);
-            if (m.find()) {
-                int year = Integer.parseInt(m.group(1));
-                int month = Integer.parseInt(m.group(2));
-                int day = Integer.parseInt(m.group(3));
-
-                int hour = Integer.parseInt(m.group(5));
-                int minute = Integer.parseInt(m.group(6));
-                int second = Integer.parseInt(m.group(7));
-
-                Calendar o = new GregorianCalendar(TimeZone.getTimeZone("Etc/UTC"));
-                o.set(Calendar.YEAR, year);
-                o.set(Calendar.MONTH, month - 1);
-                o.set(Calendar.DAY_OF_MONTH, day);
-                o.set(Calendar.HOUR_OF_DAY, hour);
-                o.set(Calendar.MINUTE, minute);
-                o.set(Calendar.SECOND, second);
-                o.set(Calendar.MILLISECOND, 0);
-                return o;
-            }
-        } catch (NumberFormatException ignored) {}
-
-        return null;
-    }
-
-    @Nullable
-    public static ProxmarkVersion parse(@Nullable String s) {
-        if (s == null) {
-            return null;
-        }
-
-        ProxmarkVersion v = new ProxmarkVersion();
-        // References:
-        //  - mainline:
-        //    SendVersion: https://github.com/proxmark/proxmark3/blob/master/armsrc/appmain.c
-        //  - iceman:
-        //    SendVersion: https://github.com/iceman1001/proxmark3/blob/master/armsrc/appmain.c
-
-        String lowerS = s.toLowerCase(Locale.ENGLISH);
-
-        if ((lowerS.contains("version information appears invalid") ||
-                lowerS.contains("version information not available"))) {
-            v.mBranch = Branch.ERROR;
-            return v;
-        }
-
-        if (lowerS.contains("taobao") || lowerS.contains("alibaba") || lowerS.contains("163.com")) {
-            v.mBranch = Branch.CHINA;
-            return v;
-        }
-
-        if ((lowerS.contains("[ arm ]") && lowerS.contains("[ fpga ]"))) {
-            // Looks like Iceman firmware.
-            // TODO: parse this string better when we can support iceman version
-            v.mBranch = Branch.ICEMAN;
-            return v;
-        }
-
-        // Lets parse up the firmware details.
-        String lines[] = s.split("\n");
-        for (String line : lines) {
-            line = line.trim();
-            if (line.startsWith("os: ") && v.mOsVersion == null) {
-                v.mOsVersion = line.substring(4);
-            }
-
-            if (line.startsWith("bootrom: ") && v.mBootromVersion == null) {
-                v.mBootromVersion = line.substring(9);
-            }
-        }
-
-        if (v.mOsVersion == null) {
-            v.mBranch = Branch.UNKNOWN;
-        } else {
-            if (v.mOsVersion.contains("iceman")) {
-                v.mBranch = Branch.ICEMAN;
-            }
-
-            if (v.mOsVersion.contains("-dirty") || v.mOsVersion.contains("-unclean")) {
-                v.mDirty = true;
-            }
-
-            if (v.mOsVersion.contains("-suspect")) {
-                v.mSuspect = true;
-            }
-
-            v.mSuperSuspect = v.mOsVersion.contains("/-suspect");
-            v.mOsBuildTime = parseIsoDateTime(v.mOsVersion);
-
-            try {
-                Matcher m = PM3_VERSION_MATCHER.matcher(v.mOsVersion);
-                if (m.find()) {
-                    v.mOsMajorVersion = Integer.parseInt(m.group(1));
-                    v.mOsMinorVersion = Integer.parseInt(m.group(2));
-                    v.mOsPatchVersion = Integer.parseInt(m.group(3));
-
-                    if (m.groupCount() == 6) {
-                        // Git version available too!
-                        v.mOsCommitCount = Integer.parseInt(m.group(5));
-                        v.mOsCommitHash = m.group(6);
-                    }
-                }
-            } catch (NumberFormatException ignored) {}
-
-            if (v.mBranch == Branch.UNKNOWN && v.mOsBuildTime != null) {
-                v.mBranch = Branch.OFFICIAL;
-            }
-        }
-
-        if (v.mBootromVersion != null) {
-            v.mBootromSuperSuspect = v.mBootromVersion.contains("/-suspect");
-            v.mBootromBuildTime = parseIsoDateTime(v.mBootromVersion);
-        }
-
-
-        return v;
+    public ProxmarkVersion(ISharedPreferences preferences) {
+        this.preferences = preferences;
     }
 
     @NonNull
@@ -234,7 +94,7 @@ public class ProxmarkVersion {
     }
 
     public boolean isSupportedVersion() {
-        if (allowAllProxmarkDevices()) return true;
+        if (preferences.allowAllProxmarkDevices()) return true;
         if (mBranch != Branch.OFFICIAL) return false;
         if (mSuperSuspect) return false;
 
